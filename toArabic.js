73cae1b6e22ce5c7bdada106141a4f0ce4c69a7c
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-14 11:58:56 +0200

    toArabic

--- toArabic.js
+++ toArabic.js
@@ -1,5 +1,32 @@
 function toArabic(romanNumber) {
-  // TODO
+  if (typeof romanNumber !== 'string' || romanNumber.length === 0) return null;
+
+  let res = 0;
+
+  const romanToArabic = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  for (let i = 0; i < romanNumber.length; i += 1) {
+    const currentRomanDigit = romanNumber[i];
+    const nextRomanDigit = romanNumber[i + 1];
+    const currentValue = romanToArabic[currentRomanDigit];
+    const nextValue = romanToArabic[nextRomanDigit];
+
+    if (nextValue > currentValue) {
+      res += nextValue - currentValue;
+      i += 1;
+    } else {
+      res += currentValue;
+    }
+  }
+  return isNaN(res) ? null : res;
 }
 
 module.exports = toArabic;
--- toArabic.spec.js
+++ toArabic.spec.js
@@ -2,4 +2,69 @@ const toArabic = require('./toArabic');
 
 // see http://www.novaroma.org/via_romana/numbers.html
 
-describe('toArabic', () => {});
+describe('toArabic', () => {
+  it('should work with I', () => {
+    expect(toArabic('I')).toBe(1);
+    expect(toArabic('II')).toBe(2);
+    expect(toArabic('III')).toBe(3);
+  });
+
+  it('should work with V', () => {
+    expect(toArabic('V')).toBe(5);
+    expect(toArabic('VI')).toBe(6);
+    expect(toArabic('VII')).toBe(7);
+    expect(toArabic('VIII')).toBe(8);
+  });
+
+  it('should work with X', () => {
+    expect(toArabic('X')).toBe(10);
+    expect(toArabic('XX')).toBe(20);
+    expect(toArabic('XV')).toBe(15);
+    expect(toArabic('XI')).toBe(11);
+    expect(toArabic('XII')).toBe(12);
+  });
+
+  it('should work with L', () => {
+    expect(toArabic('L')).toBe(50);
+    expect(toArabic('LI')).toBe(51);
+    expect(toArabic('LV')).toBe(55);
+    expect(toArabic('LX')).toBe(60);
+  });
+
+  it('should work with C', () => {
+    expect(toArabic('C')).toBe(100);
+    expect(toArabic('CII')).toBe(102);
+    expect(toArabic('CVI')).toBe(106);
+    expect(toArabic('CL')).toBe(150);
+    expect(toArabic('CLVIII')).toBe(158);
+  });
+
+  it('should work with D', () => {
+    expect(toArabic('D')).toBe(500);
+    expect(toArabic('DII')).toBe(502);
+    expect(toArabic('DLVIII')).toBe(558);
+  });
+
+  it('should work with M', () => {
+    expect(toArabic('M')).toBe(1000);
+    expect(toArabic('MXII')).toBe(1012);
+    expect(toArabic('MDXXIII')).toBe(1523);
+  });
+
+  it('should work with numebrs that require substraction', () => {
+    expect(toArabic('IV')).toBe(4);
+    expect(toArabic('IX')).toBe(9);
+    expect(toArabic('XL')).toBe(40);
+    expect(toArabic('XD')).toBe(490);
+    expect(toArabic('LM')).toBe(950);
+  });
+
+  it('should return null if conversion is impossible', () => {
+    expect(toArabic()).toBe(null);
+    expect(toArabic([])).toBe(null);
+    expect(toArabic({})).toBe(null);
+    expect(toArabic('')).toBe(null);
+    expect(toArabic(true)).toBe(null);
+    expect(toArabic('toto')).toBe(null);
+  });
+});
